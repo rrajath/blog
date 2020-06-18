6246afd3b795a4060d42a4bb8c861be7de362356
Rajath Ramakrishna <rajath@amazon.com>
2020-06-17 18:16:24 -0700

    Copy CNAME file to public/ directory

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+cp CNAME public/
 # Go To Public folder
 cd public
 
