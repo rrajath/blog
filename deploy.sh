ced72d442e188f2a059e4c4c25e7c36c425b7cf7
Rajath Ramakrishna <r.rajath@protonmail.com>
2020-06-18 19:30:58 -0700

    Remove CNAME copy hack

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-cp CNAME public/
 # Go To Public folder
 cd public
 
