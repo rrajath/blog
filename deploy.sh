ebe39ef7d0c0d33b5569963532337278e615d177
Rajath Ramakrishna <rajath@amazon.com>
2020-06-23 00:05:29 -0700

    Update deploy.sh script to make it push changes to rrajath.github.io repo

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,15 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -t ezhil # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+# Copy public folder to the repo outside the submodule
+# The issue is, submodule does not get updated when running this script either because of incorrect setup 
+# or maybe because public is part of gitignore and the remote points to blog.git instead of rrajath.github.io.git.
+# In order to make this script work properly, the public folder that is generated is copied to the repo outside
+# this folder and then committed and pushed
+cp -r public ../rrajath.github.io
+
 # Go To Public folder
-cd public
+cd ../rrajath.github.io
 
 # Add changes to git.
 git add -A
